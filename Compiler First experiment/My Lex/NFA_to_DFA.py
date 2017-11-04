6c0c6e2433cf33f4e3d7863b2e2f2627dafa9c44
151250093 <151250093@smail.nju.edu.cn>
2017-11-04 23:11:33 +0800

    dfa

--- Compiler First experiment/My Lex/NFA_to_DFA.py
+++ Compiler First experiment/My Lex/NFA_to_DFA.py
@@ -1,32 +1,131 @@
-'''
-Construction of an NFA from a Regular Expression
-using Thompson algorithm
-
-                writen by LinWenye on 2017/10/31
-'''
-
-
-class ParseTree:
-    def __init__(self, re_string):
-        state_num = 0
-        pass
-
-    def construct(self):
-        pass
-
-
-class FA:
+class DFAnode:
     def __init__(self):
-        self.top = Node(0)
-        self.current_node = self.top
+        self.state = get_state()
 
-    def find_node_recursive(self,value):
-        for item in self.top:
-
-
-class Node:
-    def __init__(self, state_num):
-        self.state = state_num
         self.next = []  # multiple next node, like LinkedList<LinkedList<>> in java
-    def connect(self,node2):
-        self.next.append(node2)
+        self.edge = []  # save with what char to turn to next
+
+    def add_edge(self, next_node, one_edge):
+        self.next.append(next_node)
+        self.edge.append(one_edge)
+
+
+class StateSet:
+    def __init__(self):
+        self.state_set_list = []  # this is a list of set; just like list(set1,set2 ...,etc)
+        self.marker = []  # this marks whether the state_set in the list has calculate the e-closure(move(A,char))
+        self.dfa = []
+
+    def is_all_marked(self):
+        if 0 not in self.marker:
+            return True
+        else:
+            return False
+
+    def get_not_marked(self):
+        for i, item in enumerate(self.marker):
+            if item == 0:
+                return i
+
+    def add_without_mark(self, new_state_set):
+        self.state_set_list.append(new_state_set)
+        self.marker.append(0)
+        self.dfa.append(DFAnode())
+
+        # def add_state_set(self, the_state_set):
+        #     if the_state_set not in self.state_set_list:
+        #         self.add_without_mark(the_state_set)
+
+
+def tran_table(nfa_start, term_tuple):
+    """
+    :param term_tuple: My_RE's terminal tuple, iterate it to find the closure
+    :param nfa_start: the start node of nfa
+    :return: DFA
+    """
+    state_set = StateSet()
+    s0 = execute_closure([nfa_start])
+    state_set.add_without_mark(s0)
+
+    for ch in term_tuple:
+        the_state_set = execute_edge_closure(s0, ch)
+        if not the_state_set:  # judge of None set
+            continue
+        if the_state_set not in state_set.state_set_list:
+            state_set.add_without_mark(the_state_set)
+
+            # not in state_set, so the state must be it's index in dfa
+            state_set.dfa[0].add_edge(state_set.dfa[state - 1], ch)
+        else:
+            next_node_index = state_set.state_set_list.index(the_state_set)
+            state_set.dfa[0].add_edge(state_set.dfa[next_node_index], ch)
+
+    state_set.marker[0] = 1  # mark as looked through
+
+    while not state_set.is_all_marked():
+        index = state_set.get_not_marked()
+        unmarked_s = state_set.state_set_list[index]
+        dfa_node = state_set.dfa[index]
+        for ch in term_tuple:
+            s_next = execute_edge_closure(unmarked_s, ch)
+            if not s_next:  # judge of None set
+                continue
+            if s_next not in state_set.state_set_list:
+                state_set.add_without_mark(s_next)
+                dfa_node.add_edge(state_set.dfa[state - 1], ch)
+            else:
+                next_node_index = state_set.state_set_list.index(s_next)
+                dfa_node.add_edge(state_set.dfa[next_node_index], ch)
+        state_set.marker[index] = 1
+
+    return state_set
+
+
+state = 0
+
+
+def get_state():
+    global state
+    res = state
+    state += 1
+    return res
+
+
+def execute_closure(nfa_node_set):
+    res_set = set()
+    # print nfa_node_set
+    for node in nfa_node_set:
+        res_set.update(_execute_closure(node))
+    return res_set
+
+
+def _execute_closure(nfa_node):
+    res_set = set()
+    res_set.add(nfa_node)
+    if nfa_node.edge == '~':
+        res_set.update(_execute_closure(nfa_node.next1))
+        if nfa_node.next2:
+            res_set.update(_execute_closure(nfa_node.next2))
+    return res_set
+
+
+def execute_edge_closure(nfa_node_set, ch):
+    """
+    :param ch:
+    :param nfa_node_set:
+    :return: epsilon-closure(move(A,char))
+    """
+
+    res = set()
+    for node in nfa_node_set:
+        edge_node = _execute_edge_closure(node, ch)
+        if edge_node:
+            res.add(edge_node)
+
+    res = execute_closure(res)
+    return res
+
+
+def _execute_edge_closure(nfa_node, ch):
+    if nfa_node.edge == ch:
+        return nfa_node.next1
--- Compiler First experiment/My Lex/RE_to_NFA.py
+++ Compiler First experiment/My Lex/RE_to_NFA.py
@@ -1,68 +1,136 @@
+# coding=utf-8
+
+"""
+Construction of an NFA from a Regular Expression
+using Thompson algorithm
+
+                writen by LinWenye on 2017/10/31
+"""
+
+
+class NFAnode:
+    # maybe the set next method should be include, thus no need to set the next node everywhere in the code
+    def __init__(self, edge=None):
+        self.edge = edge
+        self.next1 = None
+        self.next2 = None
+        self.state = get_state()
+
+
+class NFAchar:
+    """
+     NFA of character
+    """
+
+    def __init__(self, edge):
+        self.start = NFAnode(edge)
+        self.end = NFAnode()
+        self.start.next1 = self.end
+
+
+class NFAor:
+    """
+    operator :|
+    use ~ to represent ε
+    """
+
+    def __init__(self, nfa1, nfa2):
+        self.start = NFAnode('~')
+        self.start.next1 = nfa1.start
+        self.start.next2 = nfa2.start
+
+        self.end = NFAnode()
+
+        nfa1.end.edge = '~'
+        nfa1.end.next1 = self.end
+        nfa2.end.edge = '~'
+        nfa2.end.next1 = self.end
+
+
+class NFAcat:
+    """
+    operator: `
+    """
+
+    def __init__(self, nfa1, nfa2):
+        nfa1.end.edge = nfa2.start.edge
+        nfa1.end.next1 = nfa2.start.next1
+        nfa1.end.next2 = nfa2.start.next2
+
+        nfa2.start = nfa1.end
+
+        self.start = nfa1.start
+        self.end = nfa2.end
+
+
+class NFAclosure:
+    """
+    operator: *
+    """
+
+    def __init__(self, nfa):
+        self.start = NFAnode('~')
+        self.end = NFAnode()
+        self.start.next1 = nfa.start
+        self.start.next2 = self.end
+
+        nfa.end.edge = '~'
+        nfa.end.next1 = nfa.start
+        nfa.end.next2 = self.end
+
+
 stack_oprator = []
 stack_node = []
+state = 0
 
 
-# def compare(a, b):
-#     priority = ['|', '`', ]
-
-
-def pop_two_stack():
-    operator_char = stack_oprator.pop()
-    new_right = stack_node.pop()
-    new_left = stack_node.pop()
-    operator_char.set_children(new_left, new_right)
-    stack_node.append(operator_char)
-
-
-def re_to_parsetree(reg):
+def re_to_nfa(reg):
     cat_needed = False
     for ch in reg:
 
         if ch == '(':
             if cat_needed:
-                if not stack_oprator or stack_oprator[-1].value == '(' or stack_oprator[-1].value == '|':
-                    stack_oprator.append(BSTreeNode('`'))
+                if not stack_oprator or stack_oprator[-1] == '(' or stack_oprator[-1] == '|':
+                    stack_oprator.append('`')
 
-                elif stack_oprator[-1].value == '`':
+                elif stack_oprator[-1] == '`':
                     pop_two_stack()
-                    stack_oprator.append(BSTreeNode('`'))
+                    stack_oprator.append('`')
 
-            stack_oprator.append(BSTreeNode(ch))
+            stack_oprator.append(ch)
             cat_needed = False
         elif ch == ')':
             operator_char = stack_oprator.pop()
-            while not operator_char.value == '(':
-                if operator_char.value == '`' or operator_char.value == '|':
-                    operator_char.right = stack_node.pop()
-                    operator_char.left = stack_node.pop()
-                    stack_node.append(operator_char)
+            while not operator_char == '(':
+                if operator_char == '`' or operator_char == '|':
+                    new_nfa2 = stack_node.pop()
+                    new_nfa1 = stack_node.pop()
+                    stack_node.append(construct_nfa(operator_char, new_nfa1, new_nfa2))
                 operator_char = stack_oprator.pop()
 
             cat_needed = True
         elif ch == '*':
-            new_node = BSTreeNode(ch)
-            new_node.set_children(stack_node.pop())
-            stack_node.append(new_node)
+            stack_node.append(construct_nfa(ch, stack_node.pop()))
             cat_needed = True
         elif ch == '|':
-            if not stack_oprator or stack_oprator[-1].value == '(':
-                stack_oprator.append(BSTreeNode(ch))
+            if not stack_oprator or stack_oprator[-1] == '(':
+                stack_oprator.append(ch)
             else:
                 pop_two_stack()
 
-                stack_oprator.append(BSTreeNode(ch))
+                stack_oprator.append(ch)
 
             cat_needed = False
         else:
             if cat_needed:
-                if not stack_oprator or stack_oprator[-1].value == '(' or stack_oprator[-1].value == '|':
-                    stack_oprator.append(BSTreeNode('`'))
+                if not stack_oprator or stack_oprator[-1] == '(' or stack_oprator[-1] == '|':
+                    stack_oprator.append('`')
 
-                elif stack_oprator[-1].value == '`':
+                elif stack_oprator[-1] == '`':
                     pop_two_stack()
-                    stack_oprator.append(BSTreeNode('`'))
+                    stack_oprator.append('`')
 
-            stack_node.append(BSTreeNode(ch))
+            stack_node.append(NFAchar(ch))
             cat_needed = True
 
     while stack_oprator:
@@ -70,25 +138,46 @@ def re_to_parsetree(reg):
     return stack_node[0]
 
 
-class BSTreeNode:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
+def get_state():
+    global state
+    res = state
+    state += 1
+    return res
 
-    def set_children(self, left, right=None):
-        self.left = left
-        if right is not None:
-            self.right = right
 
-    def print_tree(self):
-        print self.value
-        if self.left:
-            self.left.print_tree()
+def pop_two_stack():
+    operator_char = stack_oprator.pop()
 
-        if self.right:
-            self.right.print_tree()
+    new_nfa2 = stack_node.pop()
+    new_nfa1 = stack_node.pop()
+
+    stack_node.append(construct_nfa(operator_char, new_nfa1, new_nfa2))
+
+
+# wrong!!! Because it's a graph, close graph. This will lead to endless recursiveness
+# the right way is to mark if the node has been seeked
+
+# def look_up_nfa(node, stat):
+#     if node.state == stat:
+#         return node
+#     else:
+#
+#         if node.next1:
+#             look_up_nfa(node.next1, stat)
+#         if node.next2:
+#             look_up_nfa(node.next2, stat)
+
+
+def construct_nfa(ch, nfa1, nfa2=None):
+    if ch == '|':
+        return NFAor(nfa1, nfa2)
+    elif ch == '`':
+        return NFAcat(nfa1, nfa2)
+    elif ch == '*':
+        return NFAclosure(nfa1)
 
 
 if __name__ == '__main__':
-    re_to_parsetree('a*|bc(d|e)').print_tree()
+    # re_to_nfa('a*|bc(d|e)')
+    re_to_nfa('(a|b)*abb')
+    # sucessfully test 2017/11/3  LinWenye, niubi!  Python, niubi!
